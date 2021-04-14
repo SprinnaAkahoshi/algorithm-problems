dffe63ec70056856fd623c23711e3bc4fee984d6
sakahoshi <sprinnaa@zillowgroup.com>
2021-04-13 21:23:39 -0700

    Completed problem 1480 of leet code

---
+++ Problem-1480/solution.js
@@ -0,0 +1,11 @@
+var runningSum = function (nums) {
+  let output = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i === 0) {
+      output.push(nums[i]);
+    } else {
+      output.push(nums[i] + output[i - 1]);
+    }
+  }
+  return output;
+};
